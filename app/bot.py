80274a5e7cba95f84d73f0bf51e2220a61d4177b
Sanjoli04 <vashisthsanjoli@gmail.com>
2025-06-27 00:56:39 +0530

    Changed the url of bot client to base tesnet url

--- app/bot.py
+++ app/bot.py
@@ -2,9 +2,10 @@ from binance import Client
 
 class BasicBot:
     def __init__(self, api_key, api_secret, testnet = True):
-        self.client = Client(api_key, api_secret)
         if testnet:
-            self.client.FUTURES_URL=self.client.FUTURES_TESTNET_URL
+            self.client = Client(api_key, api_secret, base_url='https://testnet.binancefuture.com')
+        else:
+            self.client = Client(api_key, api_secret)
     def get_balance(self):
         return self.client.futures_account_balance()
     def create_order(self,**args):

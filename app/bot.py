0ad8eaacc706490bba1ee00ef0c204709d68a645
Sanjoli04 <vashisthsanjoli@gmail.com>
2025-06-27 01:14:10 +0530

    Revert back to stable version of python binance and changed the code specifically for BasicBot __init__ function to change the url of the client.

--- app/bot.py
+++ app/bot.py
@@ -2,10 +2,9 @@ from binance import Client
 
 class BasicBot:
     def __init__(self, api_key, api_secret, testnet = True):
+        self.client = Client(api_key, api_secret)
         if testnet:
-            self.client = Client(api_key, api_secret, base_url='https://testnet.binancefuture.com')
-        else:
-            self.client = Client(api_key, api_secret)
+            self.client.FUTURES_URL=self.client.FUTURES_TESTNET_URL
     def get_balance(self):
         return self.client.futures_account_balance()
     def create_order(self,**args):
